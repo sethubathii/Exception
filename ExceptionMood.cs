56537d4e768989f27fa9a8f9699ccc8e812c3f81
sethu <sethu@DESKTOP-24CTG5U>
2021-10-07 16:16:48 +0530

    Add project files.

---
+++ ExceptionMood.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeException
+{
+    public class ExceptionMood
+    {
+        public string message;
+
+        public ExceptionMood(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyzeMood()
+        {
+
+            if (message.ToLower().Contains("happy"))//Condition to check if my Message contain Happy Word or Not
+            {
+                return "happy";
+            }
+            else if (message.ToLower().Contains("sad"))//Condition to check if my Message contain Happy Word or Not
+            {
+                return "sad";
+            }
+            else
+            {
+                return "Nither Happy nor Sad";
+            }
+
+        }
+    }
+}
